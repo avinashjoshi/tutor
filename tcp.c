ee89eca887e5c0d1981c0b3a4d288beb13892dda
Sandeep Shenoy <sandy.stonecold316@gmail.com>
2011-10-09 00:22:21 -0500

    tcp code

---
+++ tcp.c
@@ -0,0 +1,13 @@
+/*
+ * tcp.c: Code to create persistent TCP Connection with a tutor once client's join request is accepted.
+ * This file is a part of Tutor
+ *
+ * Tutor is an application to create a tutor relationship tree using the client-server model.
+ * Consists of a tutor node (server) and many student nodes (clients).
+ * This was created as a part of CS 6390 Advanced Computer Networks course at UT Dallas.
+ *
+ * Contributors: Avinash Joshi <axj107420@utdallas.edu>, Sandeep Shenoy <sxs115220@utdallas.edu>
+ */
+#include"myheader.h"
+
+
